9ac59065711a025ada6a8f80e1a4cc548413b7b3
Kioberry <1046880511@qq.com>
2025-08-07 11:53:12 -0700

    Initial Commit

---
+++ Fridge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Fridge
+//
+//  Created by 谢宛蓉 on 2025/8/7.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
